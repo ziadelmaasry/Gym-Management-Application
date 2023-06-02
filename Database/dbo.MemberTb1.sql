a4787025c336563e214a363510d836db47c407c8
ziadelmaasry <111492580+ziadelmaasry@users.noreply.github.com>
2023-06-02 18:03:55 +0300

    Add files via upload

---
+++ Database/dbo.MemberTb1.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[MemberTb1] (
+    [Id]      INT          IDENTITY (1, 1) NOT NULL,
+    [MName]   VARCHAR (50) NOT NULL,
+    [MPhone]  VARCHAR (50) NOT NULL,
+    [MAge]    INT          NOT NULL,
+    [MAmount] INT          NOT NULL,
+    [MTiming] VARCHAR (50) NOT NULL,
+    [MGender] NCHAR(10) NOT NULL, 
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ Database/dbo.Payment.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Payment] (
+    [Id]            INT          NOT NULL IDENTITY,
+    [PName]         VARCHAR (50) NOT NULL,
+    [PProtein]      INT          NOT NULL,
+    [PCreatine]     INT          NOT NULL,
+    [PGlutamine]    INT          NOT NULL,
+    [PCarnitine]    INT          NOT NULL,
+    [PMultivitamin] INT          NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
